b7901a58ab44ac2a4ef557b925f32ba859bf6b6c
Mark Litwintschik <mark@marksblogg.com>
2014-10-30 01:37:10 -0700

    Took distribute out. File storage is now in a temp folder which is removed afterward

--- candidate/tests.py
+++ candidate/tests.py
@@ -1,10 +1,13 @@
 from os.path import abspath, join, dirname
+from shutil import rmtree
+from tempfile import mkdtemp
+
 
 from django.core.files.storage import FileSystemStorage
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
-
+from django.test.utils import override_settings
 import mock
 
 
@@ -12,14 +15,20 @@ class ViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-    
+        self.media_folder = mkdtemp()
+
+    def tearDown(self):
+        rmtree(self.media_folder)
+
     @mock.patch('storages.backends.s3boto.S3BotoStorage', FileSystemStorage)
     def test_post_photo(self):
         photo_path = join(abspath(dirname(__file__)), 'fixtures/gradient.jpg')
+
         with open(photo_path) as photo:
-            resp = self.client.post(reverse('create'), {
-                'first_name': 'Test',
-                'photo': photo
-            })
-            redirect = 'Location: http://testserver%s' % reverse('created')
-            self.assertTrue(redirect in str(resp))
+            with override_settings(MEDIA_ROOT=self.media_folder):
+                resp = self.client.post(reverse('create'), {
+                    'first_name': 'Test',
+                    'photo': photo
+                })
+                redirect = 'Location: http://testserver%s' % reverse('created')
+                self.assertTrue(redirect in str(resp))
